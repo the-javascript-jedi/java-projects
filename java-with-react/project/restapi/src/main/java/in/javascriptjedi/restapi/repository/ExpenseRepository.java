9219fafcda334dd955e20f670f3ebf8a12126a45
nithin.thewebdev <nithin.thewebdev@gmail.com>
2025-06-24 08:45:32 +0530

    added jpa entity

---
+++ java-with-react/project/restapi/src/main/java/in/javascriptjedi/restapi/repository/ExpenseRepository.java
@@ -0,0 +1,6 @@
+package in.javascriptjedi.restapi.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ExpenseRepository extends JpaRepository<ExpenseRepository,Long> {
+}
