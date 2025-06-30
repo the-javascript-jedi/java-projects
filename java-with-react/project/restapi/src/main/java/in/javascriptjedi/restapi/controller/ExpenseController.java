446f375380f0ed76920a4119968330e6cca57b0e
nithin.thewebdev <nithin.thewebdev@gmail.com>
2025-06-30 07:39:44 +0530

    fixed issues in expense controller and repo

--- java-with-react/project/restapi/src/main/java/in/javascriptjedi/restapi/controller/ExpenseController.java
+++ java-with-react/project/restapi/src/main/java/in/javascriptjedi/restapi/controller/ExpenseController.java
@@ -1,12 +1,35 @@
 package in.javascriptjedi.restapi.controller;
+import in.javascriptjedi.restapi.dto.ExpenseDTO;
+import in.javascriptjedi.restapi.io.ExpenseResponse;
+import in.javascriptjedi.restapi.service.ExpenseService;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @RestController
+@RequiredArgsConstructor
 public class ExpenseController {
 
+    private final ExpenseService expenseService;
+    private final ModelMapper modelMapper;
+
     @GetMapping("/expenses")
-    public String getExpenses(){
-        return "Reading the expenses from Database";
+    public List<ExpenseResponse> getExpenses(){
+        // Call the service method
+        List<ExpenseDTO> list = expenseService.getAllExpenses();
+        // Convert the Expense DTO to Expense Response
+        List<ExpenseResponse> response=list.stream().map(
+                expenseDTO -> mapToExpenseResponse(expenseDTO)
+                ).collect(Collectors.toList());
+        // Return the list/response
+        return response;
+    }
+
+    private ExpenseResponse mapToExpenseResponse(ExpenseDTO expenseDTO){
+       return modelMapper.map(expenseDTO, ExpenseResponse.class);
     }
 }
--- java-with-react/project/restapi/src/main/java/in/javascriptjedi/restapi/repository/ExpenseRepository.java
+++ java-with-react/project/restapi/src/main/java/in/javascriptjedi/restapi/repository/ExpenseRepository.java
@@ -1,6 +1,7 @@
 package in.javascriptjedi.restapi.repository;
 
+import in.javascriptjedi.restapi.entity.ExpenseEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ExpenseRepository extends JpaRepository<ExpenseRepository,Long> {
+public interface ExpenseRepository extends JpaRepository<ExpenseEntity,Long> {
 }
