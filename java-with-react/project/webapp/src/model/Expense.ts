aec00bb192cdf6130589c7a47ed37f5460590be2
nithin.thewebdev <nithin.thewebdev@gmail.com>
2025-07-04 19:39:18 +0530

    added table

---
+++ java-with-react/project/webapp/src/model/Expense.ts
@@ -0,0 +1,8 @@
+export interface Expense{
+    id?:number
+    name:string;
+    amount:number;
+    date:string;
+    category:string;
+    note:string;
+}
--- java-with-react/project/webapp/src/pages/dashboard/Dashboard.tsx
+++ java-with-react/project/webapp/src/pages/dashboard/Dashboard.tsx
@@ -1,24 +1,31 @@
 import React from "react";
+import type { Expense } from "../../model/Expense";
 
 const Dashboard = () => {
-  const expenses = [
+  const expenses:Expense[] = [
     {
       id: 1,
       name: "Water Bill",
       amount: 200.0,
       date: new Date().toDateString(),
+      category: "Utilities",
+      note: "Monthly water bill",
     },
     {
       id: 2,
       name: "Electricity bill",
       amount: 500.0,
       date: new Date().toDateString(),
+      category: "Utilities",
+      note: "Monthly water bill",
     },
     {
       id: 3,
       name: "Wifi Bill",
       amount: 700.0,
       date: new Date().toDateString(),
+      category: "Utilities",
+      note: "Monthly water bill",
     },
   ];
   return <div>
