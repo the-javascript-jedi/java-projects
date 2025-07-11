b7ef03a8845d4a60bf6a2a8b56d899874f3c52fe
nithin.thewebdev <nithin.thewebdev@gmail.com>
2025-07-11 23:00:59 +0530

    added menu

--- java-with-react/project/webapp/src/components/Navbar.tsx
+++ java-with-react/project/webapp/src/components/Navbar.tsx
@@ -1,26 +1,36 @@
 const Navbar = () => {
   return (
-   <nav className="navbar navbar-expand-lg navbar-light bg-light">
-  <div className="container-fluid">
-    <a className="navbar-brand" href="#">Navbar</a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    <nav className="navbar navbar-expand-lg navbar-light">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#">
+          Navbar
+        </a>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <div className="navbar-nav">
+            <a className="nav-link active" aria-current="page" href="#">
+              Home
+            </a>
+            <a className="nav-link" href="#">
+              Features
+            </a>
+            <a className="nav-link" href="#">
+              Pricing
+            </a>
+            <a className="nav-link disabled" href="#" aria-disabled="true">
+              Disabled
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className="d-flex">
+        <button className="btn btn-sm btn-outline-light">Login</button>
+        <button className="btn btn-sm btn-outline-light mx-1">Logout</button>
+         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
-    <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-      <div className="navbar-nav">
-        <a className="nav-link active" aria-current="page" href="#">Home</a>
-        <a className="nav-link" href="#">Features</a>
-        <a className="nav-link" href="#">Pricing</a>
-        <a className="nav-link disabled" href="#" aria-disabled="true">Disabled</a>
       </div>
-    </div>
-  </div>
-   <form className="d-flex">
-    <button className="btn btn-sm btn-bg-light">Login</button>
-    <button className="btn btn-sm btn-bg-light">Logout</button>
-    </form>
-</nav>
-  )
-}
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
