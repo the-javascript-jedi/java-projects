45b7ef64c3bb50cb8494eefe61fd3fa7c9858872
nithin.thewebdev <nithin.thewebdev@gmail.com>
2025-07-18 11:37:21 +0530

    added logging and docs

--- java-with-react/project/restapi/src/main/java/in/javascriptjedi/restapi/service/ExpenseService.java
+++ java-with-react/project/restapi/src/main/java/in/javascriptjedi/restapi/service/ExpenseService.java
@@ -7,11 +7,16 @@ import java.util.List;
  * Service interface for Expense module
  * */
 public interface ExpenseService {
+
     /**
      * It will fetch the expenses from database
      * @return list
      * */
     List<ExpenseDTO> getAllExpenses();
-
+    /**
+     * It will fetch the single expense details from database
+     * @param expenseId
+     * @return ExpenseDTO
+     * */
     ExpenseDTO getExpenseByExpenseId(String expenseId);
 }
