7d22a5f76c83bd28bea816b05047877a279d9ef3
nithin.thewebdev <nithin.thewebdev@gmail.com>
2025-07-21 08:40:02 +0530

    added api call for single expense

--- java-with-react/project/webapp/src/pages/expense/ExpenseDetails.tsx
+++ java-with-react/project/webapp/src/pages/expense/ExpenseDetails.tsx
@@ -1,12 +1,32 @@
 import { useParams } from "react-router-dom";
 import CurrencyUtils from "../../utils/CurrencyUtils";
 import DateUtils from "../../utils/DateUtils";
+import { useEffect } from "react";
+import { getExpenseByExpenseId } from "../../services/expense-service";
 
 const ExpenseDetails = () => {
   const { expenseId } = useParams();
   console.log("Expense ID:", expenseId);
+
+  useEffect(() => {
+    if (expenseId !== undefined) {
+      console.log("expenseId", expenseId);
+      getExpenseByExpenseId(expenseId)
+        .then((response) => {
+          console.log("response", response);
+        })
+        .catch((error) => {
+          console.error("Error fetching expense details:", error);
+        });
+    }
+  }, []);
   return (
     <div className="container mt-2">
+      <div className="d-flex flex-row-reverse mb-2">
+        <button className="btn btn-sm btn-danger">Delete</button>
+        <button className="btn btn-sm btn-warning mx-2">Edit</button>
+        <button className="btn btn-sm btn-secondary">Back</button>
+      </div>
       <div className="card">
         <div className="card-body p-3">
           <table className="table table-striped table-hover table-responsive">
--- java-with-react/project/webapp/src/services/expense-service.ts
+++ java-with-react/project/webapp/src/services/expense-service.ts
@@ -1,6 +1,10 @@
-import apiClient from "../config/api-client"
-import type { Expense } from "../model/Expense"
+import apiClient from "../config/api-client";
+import type { Expense } from "../model/Expense";
 
-export const getExpenses=()=>{
-    return apiClient.get<Expense[]>('/expenses')
-}
+export const getExpenses = () => {
+  return apiClient.get<Expense[]>("/expenses");
+};
+
+export const getExpenseByExpenseId = (expenseId: string) => {
+  return apiClient.get<Expense>(`/expenses/${expenseId}`);
+};
