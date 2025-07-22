4b0ba395f341f40ea84b0d617c74bcbe9e6171d4
nithin.thewebdev <nithin.thewebdev@gmail.com>
2025-07-22 09:34:51 +0530

    integrated ui with api

--- java-with-react/project/webapp/src/pages/expense/ExpenseDetails.tsx
+++ java-with-react/project/webapp/src/pages/expense/ExpenseDetails.tsx
@@ -1,33 +1,82 @@
 import { useParams } from "react-router-dom";
 import CurrencyUtils from "../../utils/CurrencyUtils";
 import DateUtils from "../../utils/DateUtils";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { getExpenseByExpenseId } from "../../services/expense-service";
+import type { Expense } from "../../model/Expense";
 
 const ExpenseDetails = () => {
-  const { expenseId } = useParams();
+  const { expenseId } = useParams<{ expenseId: string }>();
+  const [expense, setExpense] = useState<Expense | undefined>();
+  const [error, setError] = useState<string | null>("");
+  const [isLoading, setIsLoading] = useState<boolean>(false);
   console.log("Expense ID:", expenseId);
 
   useEffect(() => {
     if (expenseId !== undefined) {
+      setIsLoading(true); // Set loading state to true
+      setError(null); // Reset error state
       console.log("expenseId", expenseId);
       getExpenseByExpenseId(expenseId)
         .then((response) => {
-          console.log("response", response);
+          setExpense(response.data);
         })
         .catch((error) => {
           console.error("Error fetching expense details:", error);
+          setError(error.message || "Failed to fetch expense details");
+        })
+        .finally(() => {
+          setIsLoading(false);
         });
     }
   }, []);
   return (
     <div className="container mt-2">
+      {isLoading && <p>Loading...</p>}
+      {error && <p className="text-danger">{error}</p>}
+
+      {/* api  */}
       <div className="d-flex flex-row-reverse mb-2">
         <button className="btn btn-sm btn-danger">Delete</button>
         <button className="btn btn-sm btn-warning mx-2">Edit</button>
         <button className="btn btn-sm btn-secondary">Back</button>
+        <div className="card">
+          <div className="card-body p-3">
+            <table className="table table-striped table-hover table-responsive">
+              <tbody>
+                <tr>
+                  <th>Name</th>
+                  <td>{expense?.name}</td>
+                </tr>
+                <tr>
+                  <th>Category</th>
+                  <td>{expense?.category}</td>
+                </tr>
+                <tr>
+                  <th>Amount</th>
+                  <td>
+                    {CurrencyUtils.formatCurrency(expense?.amount ?? 0, "INR")}
+                  </td>
+                </tr>
+                <tr>
+                  <th>Date</th>
+                  <td>
+                    {expense?.date !== undefined
+                      ? DateUtils.formatDateString(expense?.date)
+                      : "-No Data-"}
+                  </td>
+                </tr>
+                <tr>
+                  <th>Notes</th>
+                  <td>{expense?.note}</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
       </div>
-      <div className="card">
+      {/* html */}
+      {/* <div className="card">
         <div className="card-body p-3">
           <table className="table table-striped table-hover table-responsive">
             <tbody>
@@ -54,7 +103,7 @@ const ExpenseDetails = () => {
             </tbody>
           </table>
         </div>
-      </div>
+      </div> */}
     </div>
   );
 };
