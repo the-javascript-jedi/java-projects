05f3b43ab27a2c9fa2e4ae1e51b3c49357eac229
nithin.thewebdev <nithin.thewebdev@gmail.com>
2025-07-23 23:38:10 +0530

    added logging and docs

--- java-with-react/project/webapp/src/pages/expense/ExpenseDetails.tsx
+++ java-with-react/project/webapp/src/pages/expense/ExpenseDetails.tsx
@@ -4,58 +4,68 @@ import DateUtils from "../../utils/DateUtils";
 import { useEffect, useState } from "react";
 import { getExpenseByExpenseId } from "../../services/expense-service";
 import type { Expense } from "../../model/Expense";
+import useExpenseByExpenseId from "../../hooks/useExpenseByExpenseId";
 
 const ExpenseDetails = () => {
   const { expenseId } = useParams<{ expenseId: string }>();
-  const [expense, setExpense] = useState<Expense | undefined>();
-  const [error, setError] = useState<string | null>("");
-  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const { expense, error, isLoading } = useExpenseByExpenseId(expenseId ?? ""); // Custom hook to fetch expense details
+
+  // const [expense, setExpense] = useState<Expense | undefined>();
+  // const [error, setError] = useState<string | null>("");
+  // const [isLoading, setIsLoading] = useState<boolean>(false);
   console.log("Expense ID:", expenseId);
 
-  useEffect(() => {
-    if (expenseId !== undefined) {
-      setIsLoading(true); // Set loading state to true
-      setError(null); // Reset error state
-      console.log("expenseId", expenseId);
-      getExpenseByExpenseId(expenseId)
-        .then((response) => {
-          setExpense(response.data);
-        })
-        .catch((error) => {
-          console.error("Error fetching expense details:", error);
-          setError(error.message || "Failed to fetch expense details");
-        })
-        .finally(() => {
-          setIsLoading(false);
-        });
-    }
-  }, []);
+  // useEffect(() => {
+  //   if (expenseId !== undefined) {
+  //     setIsLoading(true); // Set loading state to true
+  //     setError(null); // Reset error state
+  //     console.log("expenseId", expenseId);
+  //     getExpenseByExpenseId(expenseId)
+  //       .then((response) => {
+  //         setExpense(response.data);
+  //       })
+  //       .catch((error) => {
+  //         console.error("Error fetching expense details:", error);
+  //         setError(error.message || "Failed to fetch expense details");
+  //       })
+  //       .finally(() => {
+  //         setIsLoading(false);
+  //       });
+  //   }
+  // }, []);
   return (
     <div className="container mt-2">
       {isLoading && <p>Loading...</p>}
       {error && <p className="text-danger">{error}</p>}
 
       {/* api  */}
-      <div className="d-flex flex-row-reverse mb-2">
-        <button className="btn btn-sm btn-danger">Delete</button>
-        <button className="btn btn-sm btn-warning mx-2">Edit</button>
-        <button className="btn btn-sm btn-secondary">Back</button>
+      <div className="mb-2">
+        <div className="d-flex flex-row-reverse mb-2">
+          <button className="btn btn-sm btn-danger">Delete</button>
+          <button className="btn btn-sm btn-warning mx-2">Edit</button>
+          <button className="btn btn-sm btn-secondary">Back</button>
+        </div>
         <div className="card">
           <div className="card-body p-3">
             <table className="table table-striped table-hover table-responsive">
               <tbody>
                 <tr>
                   <th>Name</th>
-                  <td>{expense?.name}</td>
+                  <td>{expense ? expense?.name : "N/A"}</td>
                 </tr>
                 <tr>
                   <th>Category</th>
-                  <td>{expense?.category}</td>
+                  <td>{expense ? expense?.category : "N/A"}</td>
                 </tr>
                 <tr>
                   <th>Amount</th>
                   <td>
-                    {CurrencyUtils.formatCurrency(expense?.amount ?? 0, "INR")}
+                    {expense
+                      ? CurrencyUtils.formatCurrency(
+                          expense?.amount ?? 0,
+                          "INR"
+                        )
+                      : "N/A"}
                   </td>
                 </tr>
                 <tr>
@@ -68,7 +78,7 @@ const ExpenseDetails = () => {
                 </tr>
                 <tr>
                   <th>Notes</th>
-                  <td>{expense?.note}</td>
+                  <td>{expense ? expense?.note : "N/A"}</td>
                 </tr>
               </tbody>
             </table>
