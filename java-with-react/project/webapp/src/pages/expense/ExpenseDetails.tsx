09014f323fa16fa4be4922fac9bee361379a1bf9
nithin.thewebdev <nithin.thewebdev@gmail.com>
2025-08-07 23:56:37 +0530

    removed html

--- java-with-react/project/webapp/src/pages/expense/ExpenseDetails.tsx
+++ java-with-react/project/webapp/src/pages/expense/ExpenseDetails.tsx
@@ -85,35 +85,6 @@ const ExpenseDetails = () => {
           </div>
         </div>
       </div>
-      {/* html */}
-      {/* <div className="card">
-        <div className="card-body p-3">
-          <table className="table table-striped table-hover table-responsive">
-            <tbody>
-              <tr>
-                <th>Name</th>
-                <td>Water Bills</td>
-              </tr>
-              <tr>
-                <th>Category</th>
-                <td>Bills</td>
-              </tr>
-              <tr>
-                <th>Amount</th>
-                <td>{CurrencyUtils.formatCurrency(500, "INR")}</td>
-              </tr>
-              <tr>
-                <th>Date</th>
-                <td>{DateUtils.formatDateString(new Date().toDateString())}</td>
-              </tr>
-              <tr>
-                <th>Notes</th>
-                <td>My first spendings</td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
-      </div> */}
     </div>
   );
 };
